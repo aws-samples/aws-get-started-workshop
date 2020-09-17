626b843b980ef2338515b80a25c73d541250aad0
ckamps <cpkampmeier@gmail.com>
2020-09-16 20:39:36 -0500

    testing multi branch changes

--- automation/content-build-deploy/deploy.sh
+++ automation/content-build-deploy/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 set -vex
-echo CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN = $CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN
 [ -n "${WEB_SITE_BUCKET}" ] || exit 1
-[ -n "${CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN}" ] || exit 1
+[ -n "${CUSTOM_CODEBUILD_GIT_CLEAN_BRANCH}" ] || exit 1
 [ -n "${CLOUDFRONT_DISTRO_ID}" ] || exit 1
 
 set +vx
@@ -11,15 +10,14 @@ set +vx
 if [ "$CUSTOM_CODEBUILD_GIT_PR" != '' ] ; then
   export DEPLOY_DIR="temp/prs/${CUSTOM_CODEBUILD_GIT_PR}";
 else
-  if [ "$CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN" != 'master' ] ; then
-    export DEPLOY_DIR="temp/branches/${CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN}";
+  if [ "$CUSTOM_CODEBUILD_GIT_CLEAN_BRANCH" != 'master' ] ; then
+    export DEPLOY_DIR="temp/branches/${CUSTOM_CODEBUILD_GIT_CLEAN_BRANCH}";
   else
     export DEPLOY_DIR='';
   fi
 fi
 echo CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN = $CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN
 echo CUSTOM_CODEBUILD_GIT_PR = $CUSTOM_CODEBUILD_GIT_PR
-echo CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN = $CUSTOM_CODEBUILD_GIT_BRANCH_CLEAN
 echo DEPLOY_DIR = $DEPLOY_DIR
 
 #aws s3 sync public/ s3://${WEB_SITE_BUCKET}/${DEPLOY_DIR} --delete;
